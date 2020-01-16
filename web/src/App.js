39bb4f245f7ceb08767e7c01d3dd30659e2bc000
Isabela Morais <isabeladabadia@gmail.com>
2020-01-16 20:30:03 -0300

    Aula 3 - Aside

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="lat">Latitude</label>
+              <input name="lat" id="lat" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="lng">Longitude</label>
+              <input name="lng" id="lng" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
   );
 }
 
